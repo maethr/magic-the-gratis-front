23d0094722909e907be05c500113231292d8509a
Miguel Bautista <bautistaperez3m@gmail.com>
2021-06-22 14:01:00 +0200

    Build fixes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ecomove-front-app app is running!');
+    expect(await page.getTitleText()).toEqual('album-collector-app app is running!');
   });
 
   afterEach(async () => {
