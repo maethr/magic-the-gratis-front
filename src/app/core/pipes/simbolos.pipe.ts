a465cb494acebd02e6045f7fc3a94b6c2350d015
Miguel Bautista <bautistaperez3m@gmail.com>
2022-02-28 20:06:21 +0100

    Fuck yeah simbolos en el texto

--- src/app/core/pipes/simbolos.pipe.ts
+++ src/app/core/pipes/simbolos.pipe.ts
@@ -13,34 +13,34 @@ export class SimbolosPipe implements PipeTransform {
   ) { }
 
   transform(value: string, ...args: unknown[]): string {
-    
-        let ocurrencias = value.match(/\{(.*?)\}/g);
-        let display_mana_cost = value.replace(/[{}]/g, '');
-        let html: string = `<span title="${display_mana_cost}">`;
 
-        let size: any = '28px';
-        if (args.length > 0) {
-          size = args[0];
+    let ocurrencias = value.match(/\{(.*?)\}/g);
+
+    let size: any = '28px';
+    if (args.length > 0) {
+      size = args[0];
+    }
+
+    if (ocurrencias) {
+      for (let i = 0; i < ocurrencias.length; i++) {
+
+        let simbolo_enc: any;
+        let cod_simbolo = ocurrencias[i];
+
+        simbolo_enc = this.simbolosService.getSimbolo(cod_simbolo);
+
+        if (simbolo_enc) {
+          // let simbolo_naked = cod_simbolo.replace(/[{}]/g, '');
+          let simbolo_text = simbolo_enc.english.charAt(0).toUpperCase() + simbolo_enc.english.slice(1);
+          value = value.replace(cod_simbolo, `<img width="${size}" height="${size}"
+                        src="${simbolo_enc.svg_uri}"
+                        alt="${simbolo_enc.english}"
+                        title="${simbolo_text}"
+                        class="" />`);
         }
+      }
+    }
 
-        if (ocurrencias) {
-          for (let i = 0; i < ocurrencias.length; i++) {
-
-            let simbolo_enc: any;
-            let cod_simbolo = ocurrencias[i];
-
-            simbolo_enc = this.simbolosService.getSimbolo(cod_simbolo);
-
-            if (simbolo_enc) {
-              console.log(simbolo_enc);
-              html += `<img width="${size}" height="${size}"
-                        src="${simbolo_enc.svg_uri}" alt="${simbolo_enc.english}"
-                        class="" />`;
-            }
-          }
-        }
-
-        html += '</span>'
-        return html;
+    return value;
   }
 }
