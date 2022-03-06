d1a252bd8e3c8940bbf2cf259c072be1094d5cbc
Miguel Bautista <bautistaperez3m@gmail.com>
2022-03-06 20:06:45 +0100

    Update buscador.component.ts

--- src/app/pages/buscador/buscador.component.ts
+++ src/app/pages/buscador/buscador.component.ts
@@ -49,10 +49,7 @@ export class BuscadorComponent implements OnInit {
 
       this.textoBuscado = params.get('txt');
       if (this.textoBuscado) {
-        this.getCartasS().subscribe(
-          () => {
-            this.cartasPagina = this.cartasBusqueda.slice(0, this.tam_pag);
-          });
+        this.getPaginaByPagScry();
       }
     });
     if (localStorage.getItem('tam_fila') != null) {
@@ -75,26 +72,7 @@ export class BuscadorComponent implements OnInit {
     if (num > 0 && this.paginador.last) {
       this.router.navigate(['buscar', this.tipoBusqueda, this.textoBuscado, this.pagina])
     }
-    this.getCartasS();
-  }
-
-  /* getCartas() {
-     this.cargando = true;
-     if (this.tipoBusqueda == "oracle") {
-       this.getByNombreGroupByOracle();
-     } else if (this.tipoBusqueda == "ilust") {
-       this.getByNombreGroupByIlust();
-     } else if (this.tipoBusqueda == "all") {
-       this.getByNombreGroupById();
-     }
-   }*/
-
-  _paginate(event) {
-    //event.first = Index of the first record
-    //event.rows = Number of rows to display in new page
-    //event.page = Index of the new page
-    //event.pageCount = Total number of pages
-    this.getCartasS(event.page + 1);
+    this.getPaginaByPagScry();
   }
 
   private tam_pag: number = 24;
@@ -102,12 +80,10 @@ export class BuscadorComponent implements OnInit {
   private pag_scry = 1;
   private indice_pag_scry = 0;
   private total_cartas_busqueda: number;
-  
-  
 
   paginate(event: any) {
     this.tam_pag = event.rows;
-    
+
     this.indice_pag_scry = ((event.page) * event.rows) % this.tam_pag_scry;
     let nueva_pag_scry = Math.floor(event.first / this.tam_pag_scry) + 1;
     console.log("PAGINACIÓN: ", event);
@@ -121,22 +97,24 @@ export class BuscadorComponent implements OnInit {
 
     if (nueva_pag_scry == this.pag_scry) {
       // Si la página de scryfall de la que vamos a leer es la misma que la que tenemos en memoria 
-      console.log("-iguales");
+      console.log("-pagina almacenada en memoria");
       this.cartasPagina = this.cartasBusqueda.slice(this.indice_pag_scry, this.indice_pag_scry + event.rows);
       console.log("-mostrando", this.cartasPagina.length, "cartas");
       console.log(this.cartasPagina);
-      
+
       if (this.indice_pag_scry + event.rows > this.tam_pag_scry) {
         // Si el indice de la pagina de scryfall es mayor que 175, es que hemos llegado al final de la pagina
         nueva_pag_scry = nueva_pag_scry + 1;
+        console.log("-fin página");
       }
     }
 
     if (nueva_pag_scry != this.pag_scry) {
       // Si la página de scryfall que necesitamos leer es diferente que la que tenemos en memoria
       console.log("-peticion pagina: ", nueva_pag_scry);
-      this.getCartasS(nueva_pag_scry).subscribe(() => {
+      this.getCartas(nueva_pag_scry).subscribe(() => {
         if (this.cartasPagina.length < event.rows) {
+          console.log("-inicio página", nueva_pag_scry);
           // En caso de que hayamos llegado al final de la pagina y no haya suficientes cartas para mostrar
           this.cartasPagina = this.cartasPagina.concat(this.cartasBusqueda.slice(0, event.rows - this.cartasPagina.length));
           console.log("mostrando", this.cartasPagina.length, "cartas");
@@ -149,38 +127,34 @@ export class BuscadorComponent implements OnInit {
           console.log(this.cartasPagina);
 
           if (this.cartasPagina.length < event.rows) {
+            console.log("-inicio página", nueva_pag_scry);
             // En caso de que, de dicha página nueva, estemos leyendo el final, y no haya suficientes cartas para mostrar
             // Esto solo puede pasar cuando se salta a otra página no contigua
             console.log("-peticion pagina: ", nueva_pag_scry + 1);
-            this.getCartasS(nueva_pag_scry + 1).subscribe(() => {
+            this.getCartas(nueva_pag_scry + 1).subscribe(() => {
               this.cartasPagina = this.cartasPagina.concat(this.cartasBusqueda.slice(0, event.rows - this.cartasPagina.length));
               console.log("mostrando", this.cartasPagina.length, "cartas");
               console.log(this.cartasPagina);
             });
           }
-          
+
         }
       });
     }
     this.pag_scry = nueva_pag_scry;
   }
-  /*} else {
-    console.log("-distintas")
-    if (this.indice_pag_scry > 0 && nueva_pag_scry == this.pag_scry + 1) {
-      this.cartasPagina = this.cartasBusqueda.slice(this.indice_pag_scry, this.indice_pag_scry += event.rows);
-      console.log("mostrando", this.cartasPagina.length, "cartas");
-      console.log(this.cartasPagina);
-    } else {
-      console.log("-peticion pagina: ", nueva_pag_scry);
-      this.getCartasS(nueva_pag_scry).subscribe(() => {
-        this.cartasPagina = this.cartasBusqueda.slice(this.indice_pag_scry, this.indice_pag_scry + this.tam_pag);
-        console.log("mostrando", this.cartasPagina.length, "cartas");
-        console.log(this.cartasPagina);
-      });
-    }
-  }*/
 
-  getCartasS(page_num?: number): Observable<any> {
+  getPaginaByPagScry(pag_scry: number = 1) {
+    console.log("START: ");
+    console.log("-to pagina: 1");
+    console.log("-pagina scryfall: ", this.pag_scry)
+    console.log("-indice pagina scryfall: ", this.indice_pag_scry)
+    this.getCartas().subscribe(() => {
+      this.cartasPagina = this.cartasBusqueda.slice(0, this.tam_pag);
+    });
+  }
+
+  getCartas(page_num?: number): Observable<any> {
     let params: SearchParams = {
       unique: this.tipoBusqueda,
       page: page_num ? page_num : 1
@@ -197,42 +171,4 @@ export class BuscadorComponent implements OnInit {
         //this.getImagenes();
       }));
   }
-
-  /*   getImagenes () {
-      this.cartasBusqueda.forEach(carta => {
-        this.cartaService.getImagenesCarta(carta).subscribe( () => {
-          this.cargando = false;
-        });
-      });
-    } */
-
-  /* getByNombreGroupByOracle () {
-     this.cartaService.getByNombreGroupByOracle(this.textoBuscado, this.pagina, this.tam_fila ** 2).subscribe(
-       response => {
-         this.cartasBusqueda = response.content as Carta[];
-         this.paginador = response;
-         this.getImagenes();
-       }
-     );
-   }
- 
-   getByNombreGroupByIlust () {
-     this.cartaService.getByNombreGroupByIlust(this.textoBuscado, this.pagina, this.tam_fila ** 2).subscribe(
-       response => {
-         this.cartasBusqueda = response.content as Carta[];
-         this.paginador = response;
-         this.getImagenes();
-       }
-     );
-   }
- 
-   getByNombreGroupById () {
-     this.cartaService.getByNombreGroupById(this.textoBuscado, this.pagina, this.tam_fila ** 2).subscribe(
-       response => {
-         this.cartasBusqueda = response.content as Carta[];
-         this.paginador = response;
-         this.getImagenes();
-       }
-     );
-   }*/
 }
