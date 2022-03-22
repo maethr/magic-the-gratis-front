cd3f10118aae0aae119cc35f2beeed9546e08e08
Miguel Bautista <bautistaperez3m@gmail.com>
2022-03-22 15:29:40 +0100

    Añadido context menu

--- src/app/layout/components/carta-mini/carta-mini.component.ts
+++ src/app/layout/components/carta-mini/carta-mini.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Carta } from 'src/app/core/models/carta';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { CartaWrap } from 'src/app/core/models/carta-wrap';
+import { AlbumWrapService } from 'src/app/core/services/local/album-wrap.service';
 
 @Component({
   selector: 'app-carta-mini',
@@ -11,14 +13,57 @@ export class CartaMiniComponent implements OnInit {
 
   @Input('carta') carta: CartaWrap;
 
-  constructor() {
+  items: MenuItem[];
+
+  constructor(
+    private router: Router,
+    private albumWrapService: AlbumWrapService
+  ) {
     console.log("INPUT CARTA: " + this.carta);
 
-   }
+
+  }
 
   ngOnInit(): void {
     console.log("INPUT CARTA ngOnInit: " + this.carta);
 
+    this.items = [
+      {
+        label: 'Ver carta',
+        icon: 'pi pi-fw pi-arrow-right',
+        command: () => {
+          this.router.navigate(this.carta.data.id ? ['/carta', this.carta.data.id, this.carta.id] : ['/carta', this.carta.data.id]);
+        }
+      },
+      {
+        label: 'Guardar carta',
+        icon: 'pi pi-fw pi-save',
+        command: () => {
+          this.albumWrapService.obtenerAlbumes().subscribe(() => {
+            this.albumWrapService.showGuardarCarta(this.carta.data);
+          });
+        }
+
+      },
+      {
+        separator: true
+      },
+      {
+        label: 'Otras versiones',
+        icon: 'pi pi-fw pi-search-plus',
+        command: () => {
+          this.router.navigate(['/buscar', 'prints', this.carta.data.name]);
+        }
+      },
+      {
+        label: 'Ver edición',
+        icon: 'pi pi-fw pi-search-plus',
+        command: () => {
+          this.router.navigate(['/buscar', 'cards', 'set:' + this.carta.data.set]);
+        }
+      }
+    ];
   }
+
 
 }
