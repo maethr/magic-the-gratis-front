5133488f040a25ce0e404bfa2bb67b99a07ea85b
Alonso <alonvineba@gmail.com>
2022-05-14 14:14:28 +0200

    Update endpoints.ts

---
+++ src/app/core/config/endpoints.ts
@@ -0,0 +1,21 @@
+
+export class Endpoints {
+
+    private static SERVER_URL = "localhost:8080";
+
+    public static GET_ALBUM_BY_ID				= Endpoints.SERVER_URL + "/collector/album/n/{id}"
+    public static GET_ALBUMS_FROM_USUARIO_PAGED	= Endpoints.SERVER_URL + "/collector/album/user/{user}"
+    public static GET_ALL_ALBUMS_FROM_USUARIO	= Endpoints.SERVER_URL + "/collector/album/user/{user}/all"
+    public static COUNT_ALBUMS_FROM_USUARIO		= Endpoints.SERVER_URL + "/collector/album/user/{user}/count"
+    public static CREATE_ALBUM					= Endpoints.SERVER_URL + "/collector/album/"
+    public static EDIT_ALBUM					= Endpoints.SERVER_URL + "/collector/album/n/{id}"
+    public static DELETE_ALBUM					= Endpoints.SERVER_URL + "/collector/album/n/{id}"
+    public static COUNT_CARTAS_ALBUM			= Endpoints.SERVER_URL + "/collector/album/n/{id}/count"
+
+    public static GET_PAGINA_FROM_ALBUM		    = Endpoints.SERVER_URL + "/collector/cartas/album/{id}/{page}"
+    public static GET_ALL_CARTAS_FROM_ALBUM	    = Endpoints.SERVER_URL + "/collector/cartas/album/{id}/all"
+    public static PUT_CARTA_IN_ALBUM		    = Endpoints.SERVER_URL + "/collector/cartas/album/{id}"
+    public static DELETE_CARTA				    = Endpoints.SERVER_URL + "/collector/cartas/n/{id}"
+    public static GET_X_CARTAS_ALEATORIAS	    = Endpoints.SERVER_URL + "/collector/cartas/welcome/{x}"
+
+}
